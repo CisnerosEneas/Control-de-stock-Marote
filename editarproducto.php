9dd68bb74fafc1762d3bf600a99dc7f837922e84
CisnerosEneas <101111243+CisnerosEneas@users.noreply.github.com>
2022-07-05 10:00:36 -0300

    0.2v

--- editarproducto.php
+++ editarproducto.php
@@ -6,7 +6,7 @@
 	$precio=$_GET['price'];
 	$stock=$_GET['stock'];
 	$id=$_GET['id'];
-	$dato=$cnn->prepare("update productos set id_tipo_procesado=?, precio=?, nombre=?, color=?, stock_disponible=? where id_producto=?;");
+	$dato=$cnn->prepare("update productos set id_tipo_procesado=?, precio=?, nombre=?, color=?, stock_disponible=?, actualizada_el=NOW() where id_producto=?;");
 	$dato->execute(array($tipo_procesado, $precio, $nombre, $color, $stock, $id));
 	header('location:verproducto.php');
 ?>
--- subirproducto.php
+++ subirproducto.php
@@ -5,8 +5,9 @@
 	$metodo=$_POST['a'];
 	$precio=$_POST['price'];
 	$stock=$_POST['stock'];
-	$a=$cnn->prepare("INSERT INTO productos(nombre,color,id_tipo_procesado,precio,stock_disponible) VALUES (?,?,?,?,?);");
-	$a->execute(array($nombre,$color,$metodo,$precio,$stock));
+	$descripcion=$_POST['descripcion'];
+	$a=$cnn->prepare("INSERT INTO productos(nombre,color,id_tipo_procesado,precio,stock_disponible,descripcion,creada_el) VALUES (?,?,?,?,?,?,?);");
+	$a->execute(array($nombre,$color,$metodo,$precio,$stock,$descripcion,$creada_el));
 	$cnn=null;
 	header('location:ingresarproducto.php');
 ?>
